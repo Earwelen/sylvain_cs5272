55c3e9d8e863772f5201d6a241c9658221416706
Sylvain R <sylvainriondet@gmail.com>
2019-04-18 16:18:34 +0800

    initial commit

---
+++ sylvain_cs5272/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# CS5272 Embedded Software Design
+# Sylvain Riondet A0178584L
+# Main script, choose task 1, 2, 3, or 4 (advanced)
+
+if __name__ == '__main__':
+    print("Idk what i should do here")
+
+
+
