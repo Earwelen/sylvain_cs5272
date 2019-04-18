dd129049ac27480666d1859112752185090b9357
Sylvain R <sylvainriondet@gmail.com>
2019-04-18 16:49:40 +0800

    added echo userspace for each power mode

--- sylvain_cs5272/set_chmod.sh
+++ sylvain_cs5272/set_chmod.sh
@@ -1,5 +1,12 @@
+#!/usr/bin/env bash
 # set all necessary permissions
 sudo chmod 666 /sys/devices/system/cpu/cpufreq/policy*/scaling_governor
+echo userspace > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+echo userspace > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
+
 sudo chmod 666 /sys/devices/system/cpu/cpufreq/policy*/scaling_setspeed
+
+
 sudo chmod 666 /sys/devices/platform/11800000.mali/devfreq/devfreq0/governor
+echo userspace > /sys/devices/platform/11800000.mali/devfreq/devfreq0/governor
 sudo chmod 666 /sys/devices/platform/11800000.mali/devfreq/devfreq0/userspace/set_freq
